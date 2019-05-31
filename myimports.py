5838a598e056a2f6dc2db9b8c39c0e1a955b1d16
julietteemge <48735106+julietteemge@users.noreply.github.com>
2019-05-31 00:40:15 -0700

    initial upload

---
+++ myimports.py
@@ -0,0 +1,11 @@
+
+#%%
+import ipywidgets as widgets
+from IPython.display import display
+import numpy as np
+import matplotlib.pyplot as plt
+import turtle
+#pip install python-dateutil 
+print('YAY!!')
+
+
