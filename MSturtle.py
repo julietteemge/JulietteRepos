9f3c789e86e22a5f3128eca1d74df3ba550396d0
julietteemge <48735106+julietteemge@users.noreply.github.com>
2019-05-31 00:42:50 -0700

    Add files via upload

---
+++ MSturtle.py
@@ -0,0 +1,15 @@
+# drawing a square using turtle;
+# one way is to repeat the lines
+# 2nd way is using the 'for' statement
+import turtle
+#turtle.forward(100)
+#turtle.right(90)
+#turtle.forward(100)
+#turtle.color('red')
+#turtle.right(90)
+#turtle.forward(100)
+#turtle.right(90)
+#turtle.forward(100)
+for steps in range(4):
+    turtle.forward(100)
+    turtle.right(90)
