392689ecf5efb5f7bdd0df785ae73b7da874d987
julietteemge <48735106+julietteemge@users.noreply.github.com>
2019-05-31 13:51:03 -0700

    added more turtle code

--- MSturtle.py
+++ MSturtle.py
@@ -13,3 +13,16 @@ import turtle
 for steps in range(4):
     turtle.forward(100)
     turtle.right(90)
+    print(steps)
+# above can also be written as the following :
+counter = 0
+while counter < 4:
+    turtle.forward(100)
+    turtle.right(90)
+    counter = counter+1
+    print(steps)
+    for steps2 in ['black', 'blue', 'green','red']:
+        for steps3 in range(5):
+            turtle.color(steps2)
+            turtle.forward(200)
+            turtle.right(360 / 3)
