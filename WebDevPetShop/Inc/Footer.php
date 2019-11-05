ec729a09150516f0418ebf94c30d00d9d60fe8be
julietteemge <48735106+julietteemge@users.noreply.github.com>
2019-11-04 21:32:12 -0800

    Initial upload of Inc files

---
+++ WebDevPetShop/Inc/Footer.php
@@ -0,0 +1,40 @@
+<footer>  
+	<table>
+	    <col width="500">
+	    <col width="500">
+	    <col width="500">
+	  <tr>
+		  <th align="left">Sandy's Hours of Operation</th>
+		  <th align="left">Emergency Clinic and recommended Veterinians</th>
+		  <th align="left">Recommended Boarding Kennels</th>
+	  </tr>
+	  <tr>    
+	      <td>Monday through Friday</td>
+	       <td><a href="http://www.petemergencycenter.com" target="_blank">24 Hr Pet Emergency</a></td>
+	      <td><a href="http://www.countryinnforpets.com" target="_blank">Country Inn For Pets</a></td>	
+	  </tr>
+	  <tr>    
+	      <td>9:00am - 5:00pm</td>
+	      <td><a href="http://www.thecountryvet.com" target="_blank">Country Vet</a></td>
+	      <td><a href="http://www.postmorekennels.com" target="_blank">Postmore Kennels</a></td>
+	  </tr>
+	  <tr>
+	      <td></td>
+	      <td><a href="http://www.belmarinanimalhospital.com" target="_blank">Bel Marin Animal Hospital</a></td>
+	      <td><a href="http://bridewellhilltopkennels.com" target="_blank">Bridwell Kennels</a></td>
+	      
+	  </tr>
+	  <tr>
+	      <td>Saturday</td>
+	      <td></td>
+	      <td></td>
+	  </tr>
+	  <tr>
+	      <td>10:00am - 2:00pm</td>
+	      <td></td>
+	      <td></td>
+	  </tr>		
+	 </table>
+	
+</footer>
+</html>
---
+++ WebDevPetShop/Inc/Header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Sandy's Pet Shop</title>
+  <link href="inc/PetShopMain.css" rel="stylesheet">
+</head>
+<body>
+  <header>
+	<hgroup> 
+	  <h1>Hair Salon for Dogs and Cats in Novato</h1>
+	  <h2>777 San Marin Drive</h2>
+	</hgroup>
+	<img id="logo" src="Images/logo.jpg" alt="Logo">
+	  <div class="clear"></div>
+  </header>
+  <nav id="mainnav">
+      <ul>
+	<br/>
+	  <li><a href="index.php">Home</a></li>
+	  <li><a href="aboutus.php">About Us</a></li>
+	  <li><a href="location.php">Store Location</a></li>
+	  <li><a href="grooming.php">Grooming</a></li>
+	  <li><a href="contactMessage.php">Contact Us</a></li>
+      </ul>
+  </nav>
+
